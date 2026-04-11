91fad6ea1ce572b4ec6ba9b1d505ba2126d4c6a7
Mohammad wasayullah <wasayullahmohammad@gmail.com>
2026-04-11 16:04:54 +0500

    Auto update

--- script.js
+++ script.js
@@ -997,6 +997,10 @@ document.getElementById("ch-14_question_7").innerHTML = output;
 */  
 var Name = prompt("Enter your name");
 var Score = prompt("Enter your score")
+var Names =[]
+var Scores =[]
+Names.push(Name)
+
 
 var names = ["Mohammad","Ali","Umer"]
 var score = [408,278,409]
